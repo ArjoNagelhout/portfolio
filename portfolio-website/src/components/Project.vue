27bcae5ef3f1fb587e796eedba98a2ae3216fef5
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-04-10 12:36:20 +0200

    Renamed component and added project importing

---
+++ portfolio-website/src/components/Project.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h1 class="type_project_title">{{title}}</h1>
+		<h2 class="type_project_date">{{display_date}}</h2>
+		<p class="type_paragraph">{{description}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Project',
+	props: {
+		title: String,
+		display_date: String,
+		description: String,
+		images: Array
+	}
+}
+</script>
+
+<style>
+</style>
